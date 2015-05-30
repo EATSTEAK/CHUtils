f14cc8c5915c0c3411932b1756adbc6b210d2869
itstake <bexco2010@gmail.com>
2015-05-31 00:54:13 +0900

    1.0 release, add tps function

---
+++ src/main/java/tk/itstake/chutils/LifeCycle.java
@@ -0,0 +1,27 @@
+package tk.itstake.chutils;
+
+import com.laytonsmith.PureUtilities.SimpleVersion;
+import com.laytonsmith.PureUtilities.Version;
+import com.laytonsmith.core.extensions.AbstractExtension;
+import com.laytonsmith.core.extensions.MSExtension;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import tk.itstake.chutils.utils.Lag;
+
+@MSExtension("CHActionBar")
+public class LifeCycle extends AbstractExtension {
+    @Override
+    public void onStartup() {
+        System.out.println("CHActionBar " + getVersion() + " loaded.");
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask((Plugin) this, new Lag(), 100L, 1L);
+    }
+    
+    @Override
+    public void onShutdown() {
+        System.out.println("CHActionBar " + getVersion() + " unloaded.");
+    }
+
+    public Version getVersion() {
+        return new SimpleVersion(1,0,0);
+    }
+}
